89afa968e9f126fcd1654131c9b011f5745e285d
velamurisravya <velamurisravya@gmail.com>
2025-07-30 19:07:28 +0000

    Test -- Added to my comment

--- src/components/spendwise/update-parts.tsx
+++ src/components/spendwise/update-parts.tsx
@@ -1,5 +1,5 @@
 "use client";
-//test sravya
+//test sravya again
 import type { Part, Supplier, PartSupplierAssociation, PartCategoryMapping } from '@/types/spendwise';
 // importing type { SpendDataPoint } from '@/app/page'; // SpendDataPoint not used here anymore
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
